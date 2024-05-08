b5c060c565c07cac8246ffefa4b1f9bb48791aee
Joas Fyllipe <joassolano2908@gmail.com>
2024-05-07 22:59:12 -0300

    String Multiplas Linhas

---
+++ manipulando-strings/string-multiplas-linhas/strings-triplas.py
@@ -0,0 +1,25 @@
+nome = "Joás"
+
+mensagem = print(f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+""")
+
+mensagem = print(f'''
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.
+''')
+
+print( '''
+    ============= MENU =============
+
+    1 - SACAR
+    2 - DEPOSITAR
+    0 - SAIR
+    ================================
+      
+     
+    OBRIGADO POR USAR NOSSO SISTEMA!!!
+
+''')
