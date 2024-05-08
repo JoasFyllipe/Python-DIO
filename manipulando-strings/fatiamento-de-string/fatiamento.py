db6eac65707f975191502b365fdf7cf8e335d3f3
Joas Fyllipe <joassolano2908@gmail.com>
2024-05-07 22:58:42 -0300

    Fatiamento de String

---
+++ manipulando-strings/fatiamento-de-string/fatiamento.py
@@ -0,0 +1,10 @@
+nome = "Joás Fyllipe Vieira Solano"
+
+print(nome[0]) # retorna o primeiro caractere da string
+print(nome[-1])# retorna o caractere do final para o inicio.
+print(nome[:9])# retorna do primeiro caractere iniciando no 0, até o caractere informado, sendo o numero informado exclusivo.
+print(nome[10:])# retorna o inicio no caractere informado, até o final da string.
+print(nome[10:16]) # retorna o inicio no caractere informado a esquerda, até o caractere informado na direita.
+print(nome[10:16:2]) # retorna o inicio e fim, e o ultimo numero informa de quantos em quantos será feita a pesca(caractere que caiu) da string.
+print(nome[:])# sem start e sem stop, retorna a copia da string.
+print(nome[::-1])# sem start e sem stop,e com o valor -1, retorna o espelhamento da string, a copia dela porem com os caracteres de tras para frente.
